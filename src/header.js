6e7960a080e5602f0b755ddd3cf60a32bce26789
prakhart07 <prakhart073@gmail.com>
2024-07-25 21:00:14 +0530

    header

--- src/header.js
+++ src/header.js
@@ -1,6 +1,14 @@
-
+import { useState } from 'react';
+import './css/style.css';
 
 function Header(){
+
+    const [act,setAct]=useState('');
+
+    function formaction(){
+        setAct('login-form active');
+        console.log("act",act);
+    }
 
     return(
         <div>
@@ -9,21 +17,21 @@ function Header(){
       We<span>Build</span>
     </a>
     <nav className="navbar">
-      <a href="#home">home</a>
-      <a href="#about">about</a>
-      <a href="#services">services</a>
-      <a href="#projects">projects</a>
-      <a href="#pricing">pricing</a>
-      <a href="#contact">contact</a>
-      <a href="#blogs">blogs</a>
+      <a href="#home">Home</a>
+      <a href="#about">About</a>
+      <a href="#services">Services</a>
+      <a href="#projects">Projects</a>
+      <a href="#pricing">Pricing</a>
+      <a href="#contact">Contact</a>
+      <a href="#blogs">Blogs</a>
     </nav>
     <div className="icons">
       <div id="menu-btn" className="fas fa-bars" />
       <div id="info-btn" className="fas fa-info-circle" />
       <div id="search-btn" className="fas fa-search" />
-      <div id="login-btn" className="fas fa-user" />
+      <div id="login-btn" className="fas fa-user" onClick={formaction}><button onClick={formaction} ></button></div>
     </div>
-    <form action="" className="search-form">
+    <form action={act} className="search-form">
       <input
         type="search"
         name=""
@@ -32,14 +40,11 @@ function Header(){
       />
       <label htmlFor="search-box" className="fas fa-search" />
     </form>
-    <form action="" className="login-form">
+   
+    <form action={act} className="login-form">
       <h3>login form</h3>
       <input type="email" placeholder="enter your email" className="box" />
-      <input
-        type="password"
-        placeholder="enter your password"
-        className="box"
-      />
+      <input type="password" placeholder="enter your password" className="box"/>
       <div className="flex">
         <input type="checkbox" name="" id="remember-me" />
         <label htmlFor="remember-me">remember me</label>
@@ -50,6 +55,7 @@ function Header(){
         don't have an account <a href="#">create one!</a>
       </p>
     </form>
+   
   </header>
   <div className="contact-info">
     <div id="close-contact-info" className="fas fa-times" />
@@ -77,6 +83,7 @@ function Header(){
       <a href="#" className="fab fa-linkedin" />
     </div>
   </div>
+  
         </div>
     )
 };
